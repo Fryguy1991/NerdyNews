a71cd845351b0cc414ceb15f7688f569e7f3b6af
Fryguy1991 <fryman1991@gmail.com>
2019-04-10 01:12:01 -0500

    Removed instantiation from NewsService.

--- app/src/main/java/com/chrisfry/nerdnews/business/network/NewsService.kt
+++ app/src/main/java/com/chrisfry/nerdnews/business/network/NewsService.kt
@@ -1,36 +1,16 @@
 package com.chrisfry.nerdnews.business.network
 
 import com.chrisfry.nerdnews.model.ArticleResponse
-import okhttp3.OkHttpClient
 import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
-import javax.inject.Inject
 
+/**
+ * Retrofit interface for sending responses to NewsAPI
+ */
 interface NewsService {
     companion object {
-        private const val NEWS_WEB_API_ENDPOINT = "https://newsapi.org/v2/"
-
-        @Volatile private var instance: NewsService? = null
-
-        @Inject
-        @Synchronized
-        fun getInstance(): NewsService {
-            return instance ?: synchronized(this) {
-                // Create OkHttpClient to add interceptor that adds headers to all our calls
-                val client = OkHttpClient.Builder().addNetworkInterceptor(HeaderInterceptor()).build()
-
-                val retrofit = Retrofit.Builder()
-                    .client(client)
-                    .baseUrl(NEWS_WEB_API_ENDPOINT)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
-
-                instance ?: retrofit.create(NewsService::class.java)
-            }
-        }
+        const val NEWS_WEB_API_ENDPOINT = "https://newsapi.org/v2/"
 
         // Request parameter keys
         const val KEY_CATEGORY = "category"
