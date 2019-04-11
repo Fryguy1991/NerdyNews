22a5c06caf689273ae331d635ebc1d764a87373c
Fryguy1991 <fryman1991@gmail.com>
2019-04-11 12:49:19 -0500

    Listen to recycler view in ArticleListFragment and fire events to get more articles (currently handled by NewsPagingPresenter). Once retrieved display the new articles.

--- app/src/main/java/com/chrisfry/nerdnews/business/presenters/NewsPagingPresenter.kt
+++ app/src/main/java/com/chrisfry/nerdnews/business/presenters/NewsPagingPresenter.kt
@@ -6,6 +6,7 @@ import com.chrisfry.nerdnews.business.enums.NewsApiCountrys
 import com.chrisfry.nerdnews.business.enums.NewsApiLanguages
 import com.chrisfry.nerdnews.business.eventhandling.*
 import com.chrisfry.nerdnews.business.eventhandling.events.ArticleRefreshCompleteEvent
+import com.chrisfry.nerdnews.business.eventhandling.events.MoreArticleEvent
 import com.chrisfry.nerdnews.business.eventhandling.events.RequestMoreArticleEvent
 import com.chrisfry.nerdnews.business.eventhandling.receivers.RequestMoreArticleEventReceiver
 import com.chrisfry.nerdnews.business.network.NewsCallback
@@ -21,14 +22,14 @@ import com.chrisfry.nerdnews.utils.AppUtils
 import com.chrisfry.nerdnews.utils.LogUtils
 import java.util.*
 import javax.inject.Inject
+import kotlin.collections.HashMap
 
 /**
  * Presenter for displaying a view that displays a paging list for news article types.
  * This presenters handles article refresh events that will come form the UI.
  */
 class NewsPagingPresenter private constructor() : BasePresenter<NewsPagingPresenter.INewsPagingView>(),
-    INewsPagingPresenter,
-    RequestMoreArticleEventReceiver {
+    INewsPagingPresenter, RequestMoreArticleEventReceiver {
     companion object {
         private val TAG = NewsPagingPresenter::class.java.name
 
@@ -44,6 +45,7 @@ class NewsPagingPresenter private constructor() : BasePresenter<NewsPagingPresen
         }
     }
 
+    // TODO: Suggest moving NewsService access to model so we don't have to fire events between presenters
     // Service for making calls for article data
     @Inject
     lateinit var newsService: NewsService
@@ -61,7 +63,7 @@ class NewsPagingPresenter private constructor() : BasePresenter<NewsPagingPresen
     fun initialArticleCheck() {
         // Add a refresh flag for each article type
         for (articleType: ArticleDisplayType in ArticleDisplayType.values()) {
-            // If model article list is empty mark refresh as in progress (will refresh articles in attach)
+            // If model article list is empty mark refresh as in progress
             refreshInProgressFlagList.add(articleModelInstance.getArticleList(articleType).isEmpty())
         }
 
@@ -96,7 +98,7 @@ class NewsPagingPresenter private constructor() : BasePresenter<NewsPagingPresen
     override fun onReceive(event: BaseEvent) {
         when (event is RequestMoreArticleEvent) {
             true -> {
-                // TODO: Request more articles for given article type
+                requestMoreArticles(event.articleDisplayType)
             }
             else -> {
                 LogUtils.error(TAG, "Not handling this event here: ${event::class.java.name}")
@@ -128,25 +130,81 @@ class NewsPagingPresenter private constructor() : BasePresenter<NewsPagingPresen
             refreshInProgressFlagList[i] = true
         }
 
+        // Reset page counts in model
+        for (articleType: ArticleDisplayType in ArticleDisplayType.values()) {
+            articleModelInstance.setPageCount(articleType, 0)
+        }
+
         // Request technology articles
-        val techParams = getDefaultQueryParams()
-        techParams[NewsService.KEY_CATEGORY] = NewsService.TECH_CATEGORY
-        newsService.getTopHeadlines(techParams).enqueue(ArticleRefreshCallback(ArticleDisplayType.TECH))
+        newsService.getTopHeadlines(getCallParams(ArticleDisplayType.TECH))
+            .enqueue(ArticleRefreshCallback(ArticleDisplayType.TECH))
 
         // Request science articles
-        val scienceParams = getDefaultQueryParams()
-        scienceParams[NewsService.KEY_CATEGORY] = NewsService.SCIENCE_CATEGORY
-        newsService.getTopHeadlines(scienceParams).enqueue(ArticleRefreshCallback(ArticleDisplayType.SCIENCE))
+        newsService.getTopHeadlines(getCallParams(ArticleDisplayType.SCIENCE))
+            .enqueue(ArticleRefreshCallback(ArticleDisplayType.SCIENCE))
 
-        // Request gaming articles, gaming does not have a category so we cannot use top headlines. Instead
-        // we'll request all articles from domains that generally provide gaming news (see GAMING_DOMAINS)
-        // By default NewsAPI sorts these by published by date
-        val gamingParams = HashMap<String, String>()
-        gamingParams[NewsService.KEY_LANGUAGE] = NewsApiLanguages.getLanguage(Locale.getDefault().language).code
-        gamingParams[NewsService.KEY_DOMAINS] = AppUtils.buildCommaSeparatedString(GAMING_DOMAINS)
-        gamingParams[NewsService.KEY_EXCLUDE_DOMAINS] = AppUtils.buildCommaSeparatedString(GAMING_DOMAINS_EXCLUDE)
-        gamingParams[NewsService.KEY_PAGE_SIZE] = getPageSize().toString()
-        newsService.getEverything(gamingParams).enqueue(ArticleRefreshCallback(ArticleDisplayType.GAMING))
+        // Request gaming articles
+        newsService.getEverything(getCallParams(ArticleDisplayType.GAMING))
+            .enqueue(ArticleRefreshCallback(ArticleDisplayType.GAMING))
+    }
+
+    private fun requestMoreArticles(articleType: ArticleDisplayType) {
+        LogUtils.debug(TAG, "Requesting more $articleType articles")
+
+        when (articleType) {
+            ArticleDisplayType.TECH ->  {
+                // Request more technology articles
+                newsService.getTopHeadlines(getCallParams(ArticleDisplayType.TECH))
+                    .enqueue(MoreArticleRefreshCallback(ArticleDisplayType.TECH))
+            }
+            ArticleDisplayType.SCIENCE -> {
+                // Request more science articles
+                newsService.getTopHeadlines(getCallParams(ArticleDisplayType.SCIENCE))
+                    .enqueue(MoreArticleRefreshCallback(ArticleDisplayType.SCIENCE))
+            }
+            ArticleDisplayType.GAMING -> {
+                // Request more gaming articles
+                newsService.getEverything(getCallParams(ArticleDisplayType.GAMING))
+                    .enqueue(MoreArticleRefreshCallback(ArticleDisplayType.GAMING))
+            }
+        }
+    }
+
+    private fun getCallParams(articleType: ArticleDisplayType): HashMap<String, String> {
+        // Get current page type and increment for more article call (THIS IS NOT ZERO BASED)
+        val pageCount = articleModelInstance.getPageCount(articleType) + 1
+
+        when (articleType) {
+            ArticleDisplayType.TECH -> {
+                // Technology article parameters
+                val techParams = getDefaultQueryParams()
+                techParams[NewsService.KEY_CATEGORY] = NewsService.TECH_CATEGORY
+                techParams[NewsService.KEY_PAGE] = pageCount.toString()
+                return techParams
+            }
+            ArticleDisplayType.SCIENCE -> {
+                // Science article parameters
+                val scienceParams = getDefaultQueryParams()
+                scienceParams[NewsService.KEY_CATEGORY] = NewsService.SCIENCE_CATEGORY
+                // Calculate page to retrieve (NEED TO ADD ONE, THIS IS NOT ZERO BASED)
+                scienceParams[NewsService.KEY_PAGE] = pageCount.toString()
+                return scienceParams
+            }
+            ArticleDisplayType.GAMING -> {
+                // Gaming article parameters, gaming does not have a category so we cannot use top headlines.
+                // Instead  we'll request all articles from domains that generally provide gaming news
+                // (see GAMING_DOMAINS). By default NewsAPI sorts these by published by date
+                val gamingParams = HashMap<String, String>()
+                gamingParams[NewsService.KEY_LANGUAGE] = NewsApiLanguages.getLanguage(Locale.getDefault().language).code
+                gamingParams[NewsService.KEY_DOMAINS] = AppUtils.buildCommaSeparatedString(GAMING_DOMAINS)
+                gamingParams[NewsService.KEY_EXCLUDE_DOMAINS] =
+                    AppUtils.buildCommaSeparatedString(GAMING_DOMAINS_EXCLUDE)
+                gamingParams[NewsService.KEY_PAGE_SIZE] = getPageSize().toString()
+                // Calculate page to retrieve (NEED TO ADD ONE, THIS IS NOT ZERO BASED)
+                gamingParams[NewsService.KEY_PAGE] = pageCount.toString()
+                return gamingParams
+            }
+        }
     }
 
     private fun getDefaultQueryParams(): HashMap<String, String> {
@@ -180,6 +238,8 @@ class NewsPagingPresenter private constructor() : BasePresenter<NewsPagingPresen
             LogUtils.debug(TAG, "Successfully retrieved $articleDisplayType articles")
             // Set articles into model
             articleModelInstance.setArticleList(articleDisplayType, response.articles)
+            // Store page count into model (first page due to refresh)
+            articleModelInstance.setPageCount(articleDisplayType, 1)
 
             handleArticleTypeRefreshCompleteEvent(articleDisplayType)
         }
@@ -188,9 +248,36 @@ class NewsPagingPresenter private constructor() : BasePresenter<NewsPagingPresen
             LogUtils.error(TAG, "Error refreshing $articleDisplayType articles")
             LogUtils.error(TAG, "CODE: ${error.code}\nMESSAGE: ${error.message}")
 
-            // Clear articles in model so user will see empty list and hopefully try again
+            // Clear articles and page count in model
             articleModelInstance.setArticleList(articleDisplayType, listOf())
+            articleModelInstance.setPageCount(articleDisplayType, 0)
             handleArticleTypeRefreshCompleteEvent(articleDisplayType)
+        }
+    }
+
+    /**
+     * Callback class for pulling more articles from NewsAPI
+     *
+     * @param articleDisplayType: The article type the callback is receiving
+     */
+    inner class MoreArticleRefreshCallback(private val articleDisplayType: ArticleDisplayType) :
+        NewsCallback<ArticleResponse>() {
+        override fun onResponse(response: ArticleResponse) {
+            LogUtils.debug(TAG, "Successfully retrieved more $articleDisplayType articles")
+            // Set articles into model
+            articleModelInstance.addToArticleList(articleDisplayType, response.articles)
+            // Store page count into model (old page count + 1)
+            articleModelInstance.setPageCount(articleDisplayType, articleModelInstance.getPageCount(articleDisplayType) + 1)
+
+            EventHandler.broadcast(MoreArticleEvent(articleDisplayType))
+        }
+
+        override fun onFailure(error: ResponseError) {
+            LogUtils.error(TAG, "Error retrieving more $articleDisplayType articles")
+            LogUtils.error(TAG, "CODE: ${error.code}\nMESSAGE: ${error.message}")
+
+            // Broadcast that more articles have been retrieved (actual check for this is in ArticleListPresenter)
+            EventHandler.broadcast(MoreArticleEvent(articleDisplayType))
         }
     }
 
