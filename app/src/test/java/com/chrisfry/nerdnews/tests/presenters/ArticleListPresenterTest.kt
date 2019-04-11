316f8ceff2c10bbfbc4667933ea28adcfd3ccb51
Fryguy1991 <fryman1991@gmail.com>
2019-04-11 14:10:29 -0500

    Add no more article available and article refresh test cases to ArticleListPresenterTest.

--- app/src/test/java/com/chrisfry/nerdnews/tests/presenters/ArticleListPresenterTest.kt
+++ app/src/test/java/com/chrisfry/nerdnews/tests/presenters/ArticleListPresenterTest.kt
@@ -17,6 +17,7 @@ import com.chrisfry.nerdnews.tests.BaseTest
 import org.junit.Assert
 import org.junit.Test
 import java.util.concurrent.TimeUnit
+import kotlin.random.Random
 
 /**
  * Class for isolating and testing ArticleListPresenter
@@ -56,9 +57,12 @@ class ArticleListPresenterTest: BaseTest() {
     private lateinit var mockArticleModel: MockArticleListsModel
     // Mock view object to interact with presenter
     private lateinit var mockArticleListView: MockArticleListView
+    // Type of the article list presenter. Used to randomize and
 
     override fun setUp() {
         super.setUp()
+
+        // TODO: Currently testing presenter with aricle type TECH, consider testing others
 
         // Instantiate presenter
         val presenter = ArticleListPresenter.getInstance(ArticleDisplayType.TECH)
@@ -188,7 +192,7 @@ class ArticleListPresenterTest: BaseTest() {
                             Assert.assertTrue(System.currentTimeMillis() - timestamp < TimeUnit.SECONDS.toMillis(1))
                         }
                         else -> {
-
+                            // Not handling in test
                         }
                     }
                 }
@@ -231,6 +235,137 @@ class ArticleListPresenterTest: BaseTest() {
             }
         }
     }
+
+    @Test
+    fun testRefreshArticles() {
+        val presenter = articleListPresenter
+        Assert.assertNotNull(presenter)
+
+        if (presenter == null) {
+            Assert.assertTrue(false)
+        } else {
+            presenter.attach(mockArticleListView)
+
+            // Nothing happpens on attach, values should be default
+            Assert.assertTrue(mockArticleListView.articleList.isEmpty())
+            Assert.assertFalse(mockArticleListView.areDisplayingNoArticles)
+            Assert.assertTrue(mockArticleListView.areMoreArticlesAvailable)
+
+            // Load 50 mock articles into the model
+            addArticlesToModelList(50, ArticleDisplayType.TECH)
+            // Notify presenter article model has been updated
+            EventHandler.broadcast(ArticleRefreshCompleteEvent())
+
+            // View should be displaying 50 articles
+            Assert.assertTrue(mockArticleListView.articleList.size == 50)
+            Assert.assertFalse(mockArticleListView.areDisplayingNoArticles)
+            Assert.assertTrue(mockArticleListView.areMoreArticlesAvailable)
+
+            // Request more articles from presenter
+            presenter.requestMoreArticles()
+            // Load 50 more mock articles into the model
+            addArticlesToModelList(50, ArticleDisplayType.TECH)
+            // Notify presenter model has been updated
+            EventHandler.broadcast(MoreArticleEvent(ArticleDisplayType.TECH))
+
+            // View should be displaying 100 articles
+            Assert.assertTrue(mockArticleListView.articleList.size == 100)
+            Assert.assertFalse(mockArticleListView.areDisplayingNoArticles)
+            Assert.assertTrue(mockArticleListView.areMoreArticlesAvailable)
+
+            // Simulate a refresh by clearing model data and adding 20 articles to model list
+            mockArticleModel.clearAllData()
+            addArticlesToModelList(20, ArticleDisplayType.TECH)
+            // Notify presenter article data has been refreshed
+            EventHandler.broadcast(ArticleRefreshCompleteEvent())
+
+            // View should be displaying the 20 new articles
+            Assert.assertTrue(mockArticleListView.articleList.size == 20)
+            Assert.assertFalse(mockArticleListView.areDisplayingNoArticles)
+            Assert.assertTrue(mockArticleListView.areMoreArticlesAvailable)
+        }
+    }
+
+    @Test
+    fun testNoMoreArticlesAvailable() {
+        val presenter = articleListPresenter
+        Assert.assertNotNull(presenter)
+
+        if (presenter == null) {
+            Assert.assertTrue(false)
+        } else {
+            presenter.attach(mockArticleListView)
+
+            // Nothing happpens on attach, values should be default
+            Assert.assertTrue(mockArticleListView.articleList.isEmpty())
+            Assert.assertFalse(mockArticleListView.areDisplayingNoArticles)
+            Assert.assertTrue(mockArticleListView.areMoreArticlesAvailable)
+
+            // Load 50 mock articles into the model
+            addArticlesToModelList(50, ArticleDisplayType.TECH)
+            // Notify presenter article model has been updated
+            EventHandler.broadcast(ArticleRefreshCompleteEvent())
+
+            // View should be displaying 50 articles
+            Assert.assertTrue(mockArticleListView.articleList.size == 50)
+            Assert.assertFalse(mockArticleListView.areDisplayingNoArticles)
+            Assert.assertTrue(mockArticleListView.areMoreArticlesAvailable)
+
+            // Request more articles from presenter
+            presenter.requestMoreArticles()
+            // Load 50 more mock articles into the model
+            addArticlesToModelList(50, ArticleDisplayType.TECH)
+            // Notify presenter model has been updated
+            EventHandler.broadcast(MoreArticleEvent(ArticleDisplayType.TECH))
+
+            // View should be displaying 100 articles
+            Assert.assertTrue(mockArticleListView.articleList.size == 100)
+            Assert.assertFalse(mockArticleListView.areDisplayingNoArticles)
+            Assert.assertTrue(mockArticleListView.areMoreArticlesAvailable)
+
+            // Request more articles from the presenter
+            presenter.requestMoreArticles()
+            // Don't modify model data and notify presenter that more article
+            // data is available (actually checked in presenter)
+            EventHandler.broadcast(MoreArticleEvent(ArticleDisplayType.TECH))
+
+            // View should still be displaying 100 articles and indicate that there are no more articles to display
+            Assert.assertTrue(mockArticleListView.articleList.size == 100)
+            Assert.assertFalse(mockArticleListView.areDisplayingNoArticles)
+            Assert.assertFalse(mockArticleListView.areMoreArticlesAvailable)
+
+            // Add receiver for RequestMoreArticleEvent
+            val receiver = object : RequestMoreArticleEventReceiver{
+                override fun onReceive(event: BaseEvent) {
+                    when (event is RequestMoreArticleEvent) {
+                        true -> {
+                            // This receiver should not be called because presenter should know not to
+                            // request more articles if it has already detected there are not more to display
+                            Assert.assertTrue(false)
+                        }
+                        else -> {
+                            // Not handling in test
+                        }
+                    }
+                }
+            }
+            EventHandler.addEventReceiver(receiver)
+
+            // Request more articles from the presenter above receiver should NOT be called
+            presenter.requestMoreArticles()
+
+            // Let's request a bunch more times to ensure calls aren't getting through
+            for (i in 0 until 1000) {
+                presenter.requestMoreArticles()
+
+                // View should still be displaying 100 articles and indicate that there are no more articles to display
+                Assert.assertTrue(mockArticleListView.articleList.size == 100)
+                Assert.assertFalse(mockArticleListView.areDisplayingNoArticles)
+                Assert.assertFalse(mockArticleListView.areMoreArticlesAvailable)
+            }
+        }
+    }
+
 
     /**
      * Function for injecting dummy data into the article model
