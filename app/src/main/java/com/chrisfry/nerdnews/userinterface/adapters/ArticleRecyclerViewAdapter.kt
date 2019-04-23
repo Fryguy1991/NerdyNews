9f2163a5f41405bcac8eac52f3ac39695ca5122a
Fryguy1991 <fryman1991@gmail.com>
2019-04-23 16:25:18 -0500

    Don't bother masking IllegalAccessException with my own exception.

--- app/src/main/java/com/chrisfry/nerdnews/userinterface/adapters/ArticleRecyclerViewAdapter.kt
+++ app/src/main/java/com/chrisfry/nerdnews/userinterface/adapters/ArticleRecyclerViewAdapter.kt
@@ -3,7 +3,6 @@ package com.chrisfry.nerdnews.userinterface.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.chrisfry.nerdnews.R
-import com.chrisfry.nerdnews.business.exceptions.InvalidPositionException
 import com.chrisfry.nerdnews.model.ArticleDisplayModel
 import com.chrisfry.nerdnews.userinterface.adapters.holders.ArticleViewHolder
 import com.chrisfry.nerdnews.userinterface.fragments.ArticleListFragment
@@ -14,7 +13,8 @@ import com.chrisfry.nerdnews.utils.LogUtils
 /**
  * Adapter for displaying Article objects in a recycler view
  */
-class ArticleRecyclerViewAdapter(private val fragment: ArticleListFragment) : BaseRecyclerViewAdapter<ArticleDisplayModel, ArticleViewHolder>(),
+class ArticleRecyclerViewAdapter(private val fragment: ArticleListFragment) :
+    BaseRecyclerViewAdapter<ArticleDisplayModel, ArticleViewHolder>(),
     ItemSelectionListener {
     companion object {
         private val TAG = ArticleRecyclerViewAdapter::class.java.simpleName
@@ -28,23 +28,19 @@ class ArticleRecyclerViewAdapter(private val fragment: ArticleListFragment) : Ba
     }
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
-        if (position < 0 || position >= itemCount) {
-            throw InvalidPositionException("Position in ArticleRecyclerAdapter was invalid: $position")
+        val articleToDisplay = itemList[position]
+
+        holder.setImageUrl(fragment, articleToDisplay.imageUrl)
+        holder.setSourceName(articleToDisplay.sourceName)
+
+        // Always display something in title field
+        if (articleToDisplay.title.isEmpty()) {
+            holder.setArticleTitle(fragment.getString(R.string.no_title_string))
         } else {
-            val articleToDisplay = itemList[position]
-
-            holder.setImageUrl(fragment, articleToDisplay.imageUrl)
-            holder.setSourceName(articleToDisplay.sourceName)
-
-            // Always display something in title field
-            if (articleToDisplay.title.isEmpty()) {
-                holder.setArticleTitle(fragment.getString(R.string.no_title_string))
-            } else {
-                holder.setArticleTitle(articleToDisplay.title)
-            }
-
-            holder.listener = this
+            holder.setArticleTitle(articleToDisplay.title)
         }
+
+        holder.listener = this
     }
 
     /**
@@ -53,11 +49,7 @@ class ArticleRecyclerViewAdapter(private val fragment: ArticleListFragment) : Ba
      * @param adapterPosition: Adapter index of the selected item
      */
     override fun onItemSelected(adapterPosition: Int) {
-        if (adapterPosition < 0 || adapterPosition >= itemCount) {
-            throw InvalidPositionException("$TAG: Invalid position received in onItemSelected")
-        } else {
-            LogUtils.debug(TAG, "Item selected with index: $adapterPosition")
-            listener?.onArticleSelected(itemList[adapterPosition])
-        }
+        LogUtils.debug(TAG, "Item selected with index: $adapterPosition")
+        listener?.onArticleSelected(itemList[adapterPosition])
     }
 }
